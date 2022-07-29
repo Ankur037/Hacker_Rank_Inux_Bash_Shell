218bc4d8b5985981d8a129afeacda7b52d074bc6
ANKUR SINGH <55833427+Ankur037@users.noreply.github.com>
2022-07-28 23:51:59 -0500

    Compute the Average

---
+++ Bash/Compute the Average.sh
@@ -0,0 +1,12 @@
+Given Nintegers, compute their average, rounded to three decimal places.
+
+read num
+ctr=$num
+sum=0
+while [ $ctr -gt 0 ]
+do
+   read x
+   sum=$((sum + x))
+   ctr=$((ctr - 1))     
+done
+printf "%.3f\n" `echo "$sum/$num" | bc -l`
